0ca2e7aa8c54e5a6988afd07e5aa919fa0b492f2
endlesserror <haozhi_deng@126.com>
2022-09-24 00:36:04 +0800

    commit

--- src/main/java/org/endless/erp/controller/SigninController.java
+++ src/main/java/org/endless/erp/controller/SigninController.java
@@ -1,10 +1,9 @@
 package org.endless.erp.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
 
 @Controller
 @RequestMapping("/user/signin")
@@ -12,8 +11,13 @@ public class SigninController {
 
     @GetMapping
     @PostMapping
-    @ResponseBody
-    public String signin(){
-        return "OK";
+    public String signin(@RequestParam("username") String username, @RequestParam("password") String password, Model model) {
+        if (!StringUtils.isEmpty(username) && "123456".equals(password)) {
+            System.out.println("123");
+            return "index";
+        } else {
+            model.addAttribute("msg", "用户名或密码错误！");
+            return "signin";
+        }
     }
 }
